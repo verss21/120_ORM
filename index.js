5cb69ae9f98474f3575feca2314b10c02c60a481
verss21 <arjunafrizal01@gmail.com>
2025-10-29 12:02:50 +0700

    tambah integrasi Sequelize serta fitur CRUD pada entitas Komik

--- index.js
+++ index.js
@@ -9,3 +9,62 @@ app.use(express.urlencoded({ extended: false }));
 app.listen(PORT, () => {
     console.log('Server started on port 3009');
 })
+db.sequelize.sync()
+.then((result) => {
+    app.listen(3009, () => {
+        console.log('Server Started');
+    })
+})
+.catch((err) => {
+    console.log(err);
+})
+
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try{
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch (error) {}
+    res.send({message: error.message});
+});
+
+app.get('/komik', async (req, res) =>{
+    try{
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    }
+    catch (error){
+        res.send({message: error.message});
+    }
+});
+
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    try{
+        const komik = await db.Komik.findByPk(id);
+        if (!komik){
+            return res.status(404).send({message: 'Komik not found'});
+        }
+        await komik.update(data);
+        res.send({message : "Komik berhasil di update", komik});
+    }
+    catch (error){
+        res.send({message: error.message});
+    }
+});
+
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    try{
+        const komik = await db.Komik.findByPk(id);
+        if (!komik){
+            return res.status(404).send({message: 'Komik not found'});
+        }
+        await komik.destroy();
+        res.send({message : "Komik berhasil dihapus", komik});
+    }
+    catch (error){
+        res.status(500).send(err)
+    }
+});
